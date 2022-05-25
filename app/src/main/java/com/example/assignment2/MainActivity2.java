4ad9dee047fd92d3faf10e73560ed5ab6e807add
Wouter Looijenga <w.looijenga@student.utwente.nl>
2022-05-25 16:29:59 +0200

    Add lib

--- app/src/main/java/com/example/assignment2/MainActivity2.java
+++ app/src/main/java/com/example/assignment2/MainActivity2.java
@@ -2,8 +2,10 @@ package com.example.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
@@ -17,7 +19,7 @@ public class MainActivity2 extends AppCompatActivity {
 
     TextView textRssi, textTrain, textCount, textScan;
     ScrollView scrollTrain, scrollRssi;
-    Button buttonStart, buttonStop2, buttonTrain2;
+    Button buttonStart, buttonStop2, buttonTrain2, buttonLocalize;
 
     Integer limit, count, rounds;
     Boolean start,suc;
@@ -27,7 +29,6 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
 
         // Integers
 
@@ -41,9 +42,29 @@ public class MainActivity2 extends AppCompatActivity {
         buttonStart = (Button) findViewById(R.id.buttonSTART);
         buttonStop2 = (Button) findViewById(R.id.buttonSTOP2);
         buttonTrain2 = (Button) findViewById(R.id.buttonTRAIN2);
+        buttonLocalize = (Button) findViewById(R.id.buttonLOCALIZE);
 
         // Set wifi manager.
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context c, Intent intent) {
+                boolean success = intent.getBooleanExtra(
+                        WifiManager.EXTRA_RESULTS_UPDATED, false);
+                if (success) {
+                    suc = true;
+                    System.out.println("Scan successful");
+                } else {
+                    suc = false;
+                    System.out.println("Old scan results");
+                }
+            }
+        };
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        getApplicationContext().registerReceiver(wifiScanReceiver, intentFilter);
 
 
 
@@ -54,5 +75,20 @@ public class MainActivity2 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
+        buttonLocalize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+
+    private void oneIteration() {
+
+    }
+
 }
